4589c5cf5ee63f7294448f72762e7bbb89a9096d
Andres Diaz <anondiaz@gmail.com>
2025-05-21 21:32:22 +0200

    In progress

--- modulos/add_person.php
+++ modulos/add_person.php
@@ -1,29 +1,29 @@
 <!-- ** Completa esto ** -->
-<h2>Añadir persona</h2>
+<h2>Formulario para añadir persona</h2>
 <form action="controladores/insert_person.php" method="get">
     <div>
-        <label for="nombre">Nombre :</label>
+        <label for="nombre">Nombre:</label>
         <input type="text" name="nombre" id="nombre" required>
     </div>
     <div>
-        <label for="apellidos">Apellidos :</label>
+        <label for="apellidos">Apellidos:</label>
         <input type="text" name="apellidos" id="apellidos" required>
     </div>
     <div>
-        <label for="contraseña">Contraseña :</label>
+        <label for="contraseña">Password:</label>
         <input type="password" name="contraseña" id="contraseña" required>
     </div>
     <div>
-        <label for="email">Email :</label>
+        <label for="email">Email:</label>
         <input type="email" name="email" id="email">
     </div>
     <div>
-        <label for="departamento">Departamento :</label>
+        <label for="departamento">Departamento:</label>
         <select name="departamento" id="departamento">
-            <option value="direccion" selected>direccion</option>
-            <option value="contabilidad">contabilidad</option>
-            <option value="ventas">ventas</option>
-            <option value="programacion">programacion</option>
+            <option value="direccion" selected>Dirección</option>
+            <option value="contabilidad">Contabilidad</option>
+            <option value="ventas">Ventas</option>
+            <option value="programacion">Programación</option>
         </select>
     </div>
     <div class="buttons">
--- modulos/editar_datos.php
+++ modulos/editar_datos.php
@@ -13,32 +13,32 @@ include_once 'controladores/departamentos.php';
 
 ?>
 
-<h2>Modificar persona</h2>
+<h2>Formulario para modificar persona</h2>
 <form action="controladores/modify_data.php" method="post">
     <input type="text" name="id" value="<?= $_GET['id'] ?>" hidden >
     <div>
-        <label for="nombre">Nombre :</label>
+        <label for="nombre">Nombre:</label>
         <input type="text" name="nombre" id="nombre"  value="<?= $_GET['nombre'] ?>" required>
     </div>
     <div>
-        <label for="apellidos">Apellidos :</label>
+        <label for="apellidos">Apellidos:</label>
         <input type="text" name="apellidos" id="apellidos" value="<?= $_GET['apellidos'] ?>" required>
     </div>
     <div>
-        <label for="contraseña">Contraseña :</label>
+        <label for="contraseña">La contraseña debe ser actualizada:</label>
         <input type="password" name="contraseña" id="contraseña" required>
     </div>
     <div>
-        <label for="email">Email :</label>
+        <label for="email">Email:</label>
         <input type="email" name="email" id="email" value="<?= $_GET['email'] ?>">
     </div>
     <div>
-        <label for="departamento">Departamento :</label>
+        <label for="departamento">Departamento:</label>
         <select name="departamento" id="departamento">
-            <option value="direccion" selected>direccion</option>
-            <option value="contabilidad">contabilidad</option>
-            <option value="ventas">ventas</option>
-            <option value="programacion">programacion</option>
+            <option value="direccion" selected>Dirección</option>
+            <option value="contabilidad">Contabilidad</option>
+            <option value="ventas">Ventas</option>
+            <option value="programacion">Programación</option>
         </select>
     </div>
         <div class="buttons">
