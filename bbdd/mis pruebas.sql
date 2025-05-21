3db454805ef733bbce4f1a5ca9bb8481087cac51
Andres <anondiaz@gmail.com>
2025-05-21 13:55:46 +0200

    In progress

---
+++ bbdd/mis pruebas.sql
@@ -0,0 +1,8 @@
+DROP DATABASE uf1844_andres_diaz;
+CREATE DATABASE IF NOT EXISTS uf1844_andres_diaz;
+USE uf1844_andres_diaz;
+
+-- Creamos la tabla de tareas
+CREATE TABLE IF NOT EXISTS tareas (
+id_tarea  INT NOT NULL AUTO_INCREMENT PRIMARY KEY
+);
